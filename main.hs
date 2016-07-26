dc78eca909339d71ca9e842918769c0f7eca4aa8
Alan Hawkins <alanpika_au@yahoo.com>
2016-07-26 17:03:24 +1000

    Update main.hs

--- main.hs
+++ main.hs
@@ -5,4 +5,4 @@ main = do
   
 
 
-unlines = (\x -> take ((length x) `div` 2) $ map (\y -> x!!y ) [0,2..]) $ groupBy (\x y -> not $ (x/='\n') /= (y /='\n'))
+unlines = map snd  $ filter fst $ zip (cycle [True,False])  $ groupBy (\x y -> not $ (x/='\n') /= (y /='\n')) 
