f72b1cc276a41b730a55054d85d7b8f19a09f25c
Alan Hawkins <alanpika_au@yahoo.com>
2016-10-20 13:44:41 +1100

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -6,11 +6,23 @@ import Text.XML.HaXml.XmlContent.Haskell
 import Extsubset
 import Data.Either
 
+create xs = do
+ files <- forM (xs) $ \x -> do
+   content <- readFile x
+   let q <- ( File (File_Attrs x) )
+ return (show $ htmlprint $ files)  
+  
+
 main :: IO ()
 main = do 
-       x <- getContents    
-       let p = (readXml x :: Either String Multifile)
-       either print processFiles p
+       xs <- getArgs
+       case xs of 
+         [] -> do
+           x <- getContents    
+           let p = (readXml x :: Either String Multifile)
+           either print processFiles p
+         '-':'c':'r':'e':'a':'t':'e':' ':xs' -> create xs' >>= putStr
+         _ -> putStr "unknown usage"
 
 processFiles (Multifile xs) = mapM_ processFile xs
 processFile (File (File_Attrs path) content) = writeFile path content
